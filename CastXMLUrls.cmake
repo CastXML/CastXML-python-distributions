e03b460b03032d00e00fcca27ae10548d22999a5
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2020-08-12 15:16:47 -0400

    appveyor: Add support for generating windows 32-bit wheel
    See #5

--- CastXMLUrls.cmake
+++ CastXMLUrls.cmake
@@ -11,8 +11,9 @@ set(linux64_binary_sha512 "a3c929ebd652fd159709826e154d566235fb12903dac04070854e
 set(macosx_binary_url    "https://data.kitware.com/api/v1/file/5ee7eb5c9014a6d84ec1f240/download")
 set(macosx_binary_sha512 "0ba8deff17b3a8f5a2cd22939ff83a864629201d8b881e44d7ed01d0fd2aa578338e3319e402c226b3648a6027a4538e8c4cd066d6fe2c49eb1a4471e803b827")
 
-set(win32_binary_url    "NA")  # Windows 32-bit binaries not available
-set(win32_binary_sha512 "NA")
-
 set(win64_binary_url    "https://data.kitware.com/api/v1/file/5ee7eb539014a6d84ec1f22a/download")
 set(win64_binary_sha512 "fa7a38dbdb71e0484cfeb255aef6d085abf23496a85051e3dccac593d9eec042fad5be72110d7f3528b424d90fbf5b5053c31f054470d691a7109c1f13776229")
+
+# See https://github.com/CastXML/CastXML-python-distributions/issues/5
+set(win32_binary_url    "${win64_binary_url}")
+set(win32_binary_sha512 "${win64_binary_sha512}")
